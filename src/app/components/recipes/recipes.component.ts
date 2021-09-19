45326f3b9d6b86ee38cf450ad8431c9851034443
Olivier Brassard <brassard.oli@gmail.com>
2021-09-19 01:55:43 -0500

    Improved UI for recipes

--- src/app/components/recipes/recipes.component.ts
+++ src/app/components/recipes/recipes.component.ts
@@ -3,77 +3,82 @@ import { Inventory, InventoryService, InventoryItem, InventoryDetails } from 'sr
 import { Recipe, RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
-  selector: 'app-recipes',
-  templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.scss']
+    selector: 'app-recipes',
+    templateUrl: './recipes.component.html',
+    styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent implements OnInit {
 
-  recipes: Recipe[] = [];
-  inventories: Inventory[] = [];
-  selectedInventory: string = "all";
-  items: InventoryItem[] = [];
-  searchString: string = "";
-  
-  private uncheckedItems: string[] = [];
+    recipes: Recipe[] = [];
+    inventories: Inventory[] = [];
+    selectedInventory: string = "all";
+    items: InventoryItem[] = [];
+    searchString: string = "";
+    filtersOpened: boolean = false;
 
-  constructor(private recipeService: RecipeService,
-              private inventoryService: InventoryService) { }
+    private checkedItems: string[] = [];
 
-  ngOnInit(): void {
-    //TODO get ingredients in search
-    this.inventoryService.getUsersInventories().subscribe(v => {
-      this.inventories = v;
-      // this.selectedInventory = this.inventories[0] ? this.inventories[0].id : "";
-      this.onInventoryChange(this.selectedInventory)
-    });
-    // this.recipeService.getRecipes('tomato').subscribe(recipes => this.recipes = recipes);
-  }
+    constructor(private recipeService: RecipeService,
+        private inventoryService: InventoryService) { }
 
-  onInventoryChange(event: string) {
-    console.log(event);
-    if (event == "all") {
-      this.items = [];
-      const promises: Promise<void | InventoryDetails>[] = [];
-      this.inventories.forEach(invent => {
-        promises.push(this.inventoryService.getInventoryDetails(invent.id).toPromise().then(inv => {
-          this.items = this.items.concat(inv.items);
-        }));
-      });
-
-      Promise.all(promises).then(() => {
-        this.searchString = this.items.map(item => item.title).join(" ");
-        this.searchForRecipes();
-      });
-
-    } else {
-      this.inventoryService.getInventoryDetails(event).subscribe(inv => {
-        this.items = inv.items;
-        this.searchString = this.items.map(item => item.title).join(" ");
-        this.searchForRecipes();
-      });
-    }
-  }
-
-  onCheckboxClick(itemId: string, event: any) {
-
-    const index = this.uncheckedItems.indexOf(itemId);
-    if (index > -1) {
-      this.uncheckedItems.splice(index, 1);
+    ngOnInit(): void {
+        //TODO get ingredients in search
+        this.inventoryService.getUsersInventories().subscribe(v => {
+            this.inventories = v;
+            // this.selectedInventory = this.inventories[0] ? this.inventories[0].id : "";
+            this.onInventoryChange(this.selectedInventory)
+        });
+        // this.recipeService.getRecipes('tomato').subscribe(recipes => this.recipes = recipes);
     }
 
-    if (event.target.checked) {
-      this.uncheckedItems.push(itemId);
+    onInventoryChange(event: string) {
+        if (event == "all") {
+            this.items = [];
+            const promises: Promise<void | InventoryDetails>[] = [];
+            this.inventories.forEach(invent => {
+                promises.push(this.inventoryService.getInventoryDetails(invent.id).toPromise().then(inv => {
+                    this.items = this.items.concat(inv.items);
+                }));
+            });
+
+            Promise.all(promises).then(() => {
+                this.searchString = this.items.map(item => item.title).join(" ");
+                this.searchForRecipes();
+            });
+
+        } else {
+            this.inventoryService.getInventoryDetails(event).subscribe(inv => {
+                this.items = inv.items;
+                this.searchString = this.items.map(item => item.title).join(" ");
+                this.searchForRecipes();
+            });
+        }
     }
 
-    this.searchString = this.items.map(item => this.uncheckedItems.includes(item.itemId) ? item.title : "").join(" ");
-    this.searchForRecipes();
-  }
+    isSelected(itemId: string): boolean {
+        return this.checkedItems.indexOf(itemId) > -1;
+    }
 
-  searchForRecipes(): void {
-    this.recipeService.getRecipes(this.searchString).subscribe(recipes => {
-      this.recipes = recipes;
-      console.log("Recipes", recipes)
-    });
-  }
+    onCheckboxClick(itemId: string) {
+        const index = this.checkedItems.indexOf(itemId);
+        if (index == -1) {
+            this.checkedItems.push(itemId);
+        } else {
+            this.checkedItems.splice(index, 1);
+        }
+
+        if (this.checkedItems.length == 0) {
+            this.searchString = "";
+            this.recipes = [];
+        } else {
+            this.searchString = this.items.filter(item => this.checkedItems.includes(item.itemId)).map(item => item.title).join(" ");
+            this.searchForRecipes();
+        }
+    }
+
+    searchForRecipes(): void {
+        this.recipeService.getRecipes(this.searchString).subscribe(recipes => {
+            this.recipes = recipes;
+        });
+    }
 }
